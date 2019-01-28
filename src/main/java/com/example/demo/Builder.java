0992c2bbaaaf66c8985a137828e78b248e32dd77
pandemicGander <dannygonline@gmail.com>
2019-01-28 21:02:34 +0000

    better to keep logic in abstract class for self registration

--- src/main/java/com/example/demo/Builder.java
+++ src/main/java/com/example/demo/Builder.java
@@ -1,19 +1,7 @@
 package com.example.demo;
 
-import javax.annotation.PostConstruct;
-
 public interface Builder {
 
-    BuilderFactory getBuilderFactory();
-    String getKey();
     String doLogic();
-
-    @PostConstruct
-    default void register(){
-        System.out.println("Adding to builder factory: "+this);
-        getBuilderFactory().addBuilder(getKey(), this);
-    }
-
-
 
 }
--- src/main/java/com/example/demo/ExtendMe.java
+++ src/main/java/com/example/demo/ExtendMe.java
@@ -3,14 +3,19 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public abstract class ExtendMe implements Builder {
 
     @Autowired
     private BuilderFactory builderFactory;
 
-    @Override
-    public BuilderFactory getBuilderFactory() {
-        return builderFactory;
+    abstract String getKey();
+
+    @PostConstruct
+    private void register(){
+        System.out.println("Adding to builder factory: "+this);
+        builderFactory.addBuilder(getKey(), this);
     }
 }
